8e384799775382633150203d778871e6be62e3ed
Rogério Silva <rogeriocsilva21@gmail.com>
2016-02-20 19:03:54 +0000

    ewq
    dsadas

---
+++ Assets/Time.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+    public Text texto;
+    float tempo = 100;
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tempo -= UnityEngine.Time.deltaTime;
+
+    }
+    void onGUI()
+    {
+        
+    }
+}
