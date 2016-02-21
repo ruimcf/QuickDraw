a4905e088344e1ab8de4b7a675de5d8816057c19
Rui Fonseca <rui.m.c.fonseca@hotmail.com>
2016-02-21 20:22:36 +0000

    added random image load

--- Assets/LoadImage.cs
+++ Assets/LoadImage.cs
@@ -1,22 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 public class LoadImage : MonoBehaviour {
     public GameObject image;
     public GameObject background;
-    public string path = "bulbasaur";
+    public List<string> lista;
+    public string escolhido;
 
 
 	// Use this for initialization
 	void Start () {
 
-
+        /*foreach (string file in System.IO.Directory.GetFiles(Application.dataPath + "/Resources/", "*.png*", SearchOption.AllDirectories))
+        {
+            if (file.EndsWith("png"))
+            {
+                Debug.Log("Adicionado o ficheiro: " + file);
+                lista.Add(Path.GetFileNameWithoutExtension(file));
+            }
+        }*/
+        lista.Add("bulbasaur");
+        lista.Add("mewtwo");
+        lista.Add("pikachu");
+        lista.Add("gengar");
+        lista.Add("charizard");
+        lista.Add("gato");
+        if(lista.Count == 0)
+        {
+            Debug.Log("Fudeu");
+        }
+        int random = Random.Range(0, lista.Count);
+        print(random);
+        escolhido = lista[random];
         image = new GameObject();
         image.name = "ObjectiveImage";
         image.transform.position = new Vector3(0, 0, 0);
         image.AddComponent<SpriteRenderer>();
-        image.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load(path, typeof(Sprite));
+        image.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load(lista[random], typeof(Sprite));
 
 
 
